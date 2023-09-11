d49341467fa2dabd90c8465fd1b003ff15e17a03
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-09-11 14:41:40 -0700

    5-no_c.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    """Replace an element in a copied list at a specific position"""
+    """Prints all integers of a list, in reverse order."""
     if not my_list:
         pass
     else:
