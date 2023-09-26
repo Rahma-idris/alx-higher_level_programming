0c20697b8db02f8c2861770a082ab4547cdeb46b
Rahmatullah Idris <idrisojo2001@gmail.com>
2023-09-25 18:59:00 -0700

    Task 0 & 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """Function that prints x elements of a list."""
+    count = 0
+
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+        except:
+            break
+        else:
+            count += 1
+
+    print()
+    return (count)
